8cc80c599cc8b94874c37e86fdc13343bae5319a
Lomowg <sokol789123@yandex.ru>
2024-07-16 20:54:18 +0300

    Added coding page basic

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/coding')
+def coding():
+    return render_template('coding.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
