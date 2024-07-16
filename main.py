b356ef17e9d2b245d962515546e541dda562bbab
glebmakc <g-makc@bk.ru>
2024-07-16 23:31:30 +0300

    added new pages

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ def index():
 def coding():
     return render_template('coding.html')
 
+@app.route('/diagnostic')
+def diagnostic():
+    return render_template('diagnostic.html')
+
+@app.route('/carplay')
+def carplay():
+    return render_template('carplay.html')
+@app.route('/mybmw')
+def mybmw():
+    return render_template('mybmw.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
