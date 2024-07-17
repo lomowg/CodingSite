6902068c8da9015173cf09cf159e3e5eadb4cfd5
Lomowg <sokol789123@yandex.ru>
2024-07-17 16:06:57 +0300

    Added email send script

---
+++ static/send-email.js
@@ -0,0 +1,12 @@
+function openYandexMail() {
+    const name = document.querySelector('input[name="your-name"]').value;
+    const email = document.querySelector('input[name="your-email"]').value;
+    const message = document.querySelector('textarea[name="your-message"]').value;
+
+    const subject = `Сообщение от ${name}`;
+    const body = `Имя: ${name}\nE-mail: ${email}\nСообщение: ${message}`;
+
+    const mailtoLink = `mailto:g-makc@bk.ru?to=&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+}
