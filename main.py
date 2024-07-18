a3a1338991690d2fc508ee032176714154a96b5c
glebmakc <g-makc@bk.ru>
2024-07-18 21:51:05 +0300

    favicon fixes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -33,6 +33,11 @@ def help():
 @app.route('/contacts')
 def contacts():
     return render_template('contacts.html')
+
+@app.route('/favicon.png')
+def favicon():
+    return send_from_directory(directory='.', path='favicon.png')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
