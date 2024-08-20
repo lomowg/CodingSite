14510292be3c20f94222fdb9d77c996458bb5a93
glebmakc <g-makc@bk.ru>
2024-08-20 14:54:59 +0300

    change mail to phone

--- static/send-email.js
+++ static/send-email.js
@@ -4,7 +4,7 @@ function openYandexMail() {
     const message = document.querySelector('textarea[name="your-message"]').value;
 
     const subject = `Сообщение от ${name}`;
-    const body = `Имя: ${name}\nE-mail: ${email}\nСообщение: ${message}`;
+    const body = `Имя: ${name}\nНомер телефона: ${email}\nСообщение: ${message}`;
 
     const mailtoLink = `mailto:bmwforce@bk.ru?to=&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
